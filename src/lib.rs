99a479c7dda0e31bd9ed7548b4bc1e61beb9c310
CommName <17132081+CommName@users.noreply.github.com>
2024-07-09 22:11:02 +0200

    Added name to wasm lib so it is destinguished from app

--- src/lib.rs
+++ src/lib.rs
@@ -16,11 +16,9 @@ mod wasm_lib {
         eframe::WebLogger::init(log::LevelFilter::Debug).ok();
         let web_options = eframe::WebOptions::default();
         
-        wasm_bindgen_futures::spawn_local(async {
-            
+        wasm_bindgen_futures::spawn_local(async move {    
             let quiz = Quiz::dummy_test();
         
-            log::info!("hello world 1");
             let start_result = eframe::WebRunner::new()
             .start(
                 "quiz_app", // hardcode it
@@ -28,9 +26,6 @@ mod wasm_lib {
                 Box::new(|cc| Box::new(QuizApp::new(cc, quiz))),
             )
             .await;
-        let args = std::env::args();
-        
-        log::info!("hello world 2 {:?}", args);
     });
     
     }
